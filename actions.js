d233e212198b9d6791ddcaa87d39a2fc745fbbb8
dwivediikanchan <159162757+dwivediikanchan@users.noreply.github.com>
2024-09-17 21:27:41 +0530

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,11 @@
+//actions.js
+const ACTIONS = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    //addd other actions as necessary
+};
+
+module.exports = ACTIONS;
